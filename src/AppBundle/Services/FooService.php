ee5f2e3a7175ea0cb3ab3501b793b488611a2bde
Adolfo Luzardo Cabrera <adolfoluzardocabrera@gmail.com>
2016-03-15 00:20:17 +0000

    Update FooService.php

--- src/AppBundle/Services/FooService.php
+++ src/AppBundle/Services/FooService.php
@@ -1,1 +1,25 @@
-<?php/** * Created by PhpStorm. * User: aluzardo * Date: 15/03/16 * Time: 00:13 */namespace AppBundle\Services;use Symfony\Component\HttpFoundation\Session\Session;class FooService {  var $session;  private function __construct( Session $session ) {    $this->session = $session;  }  public function setFooSession() {    return $this->session->set("foo", "foo");  }}
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aluzardo
+ * Date: 15/03/16
+ * Time: 00:13
+ */
+namespace AppBundle\Services;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class FooService {
+
+  var $session;
+
+  private function __construct( Session $session ) {
+    $this->session = $session;
+  }
+
+  public function setFooSession() {
+    return $this->session->set("foo", "foo");
+  }
+
+
+}
